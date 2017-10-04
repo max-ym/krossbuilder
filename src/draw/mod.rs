b38423fb24758d379802241a857df993ce34b889
Maxym Naumchyk <max.naumch@gmail.com>
2017-10-04 21:57:22 +0300

    New styles for cell letter and fn to draw it

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -15,6 +15,8 @@ pub struct PageStyle {
     pub line_color      : Color,
     pub line_width      : f64,
     pub cell_size       : f64,
+    pub cell_font_size  : f64,
+    pub cell_font_color : Color,
 }
 
 /// Drawing functions to render page elements to surface and show it when
@@ -45,10 +47,12 @@ impl Default for PageStyle {
 
     fn default() -> Self {
         PageStyle {
-            bg_color    : Color::new(1., 1., 1.),   // White
-            line_color  : Color::new(0., 0., 0.),   // Black
-            line_width  : 1.,
-            cell_size   : 48.,
+            bg_color        : Color::new(1., 1., 1.),   // White
+            line_color      : Color::new(0., 0., 0.),   // Black
+            line_width      : 1.,
+            cell_size       : 48.,
+            cell_font_size  : 10.,
+            cell_font_color : Color::new(0., 0., 0.),   // Black
         }
     }
 }
@@ -116,6 +120,50 @@ impl Drawer {
             self.cr().line_to(offset + 0.5, ver_length + 0.5);
             self.cr().stroke();
         }
+    }
+
+    /// Paint letter at specified cell.
+    fn paint_letter(&self, row: u16, col: u16, letter: char) {
+        use cairo::{FontFace, FontSlant, FontWeight};
+
+        let row = row as f64;
+        let col = col as f64;
+
+        // Cell center point relative to cell start coords.
+        let cell_center = self.style.cell_size / 2.;
+
+        // Find cell absolute coords on the grid.
+        let cell_x = col * (self.style.cell_size + self.style.line_width);
+        let cell_y = row * (self.style.cell_size + self.style.line_width);
+
+        let cr = self.cr();
+
+        // Setup font parameters.
+        cr.set_font_size(self.style.cell_font_size);
+        self.style.cell_font_color.set_source_in(cr);
+        cr.set_font_face(FontFace::toy_create(
+                "monospace",
+                FontSlant::Normal,
+                FontWeight::Normal,
+        ));
+
+        // Wrap a letter in the string.
+        let mut s = String::with_capacity(8);
+        s.push(letter);
+
+        // Calculate letter position offsets to center it.
+        let txtext = cr.text_extents(s.as_str());
+        let letter_offset_x = -txtext.width  / 2.;
+        let letter_offset_y = -txtext.height / 2.;
+        let letter_x = cell_center + letter_offset_x;
+        let letter_y = cell_center + letter_offset_y;
+
+        // Find exact absolute coords of a letter.
+        let x = cell_x + letter_x;
+        let y = cell_y + letter_y;
+
+        cr.move_to(x, y);
+        cr.show_text(s.as_str());
     }
 }
 
